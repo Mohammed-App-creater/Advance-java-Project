1c11cf0b2b350509b4f0c6a32a9f81323d8dbc0b
Mohammed Ismail <mahammedismail160@gmail.com>
2024-12-23 11:01:36 +0300

    Implement cart functionality with CartDB, CartItem, and CartServlet; add methods for managing cart items

---
+++ Oline-Shopping-Platform/src/main/java/com/olineshoppingplatform/olineshoppingplatform/Cart/CartItem.java
@@ -0,0 +1,15 @@
+package com.olineshoppingplatform.olineshoppingplatform.Cart;
+
+
+public class CartItem {
+    private int id;
+    private int userId;
+    private int productId;
+    private int quantity;
+    private String productName;
+    private double price;
+    private String imageUrl;
+
+    // Getters and Setters
+}
+
